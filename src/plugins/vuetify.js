ddfe15a8298ec954c017279fd19fde1b31ae4ba6
Gabriel Pastori <58153955+gabrielpastori1@users.noreply.github.com>
2023-11-14 12:18:21 -0300

    Delete unused dependencies

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,21 +11,8 @@ import 'vuetify/styles'
 // Composables
 import { createVuetify } from 'vuetify'
 
-
-// Labs features
-import {
-  VDataTable,
-  VDataTableServer,
-  VDataTableVirtual,
-} from "vuetify/labs/VDataTable";
-
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
-  components: {
-    VDataTable,
-    VDataTableServer,
-    VDataTableVirtual,
-  },
   theme: {
     themes: {
       light: {
